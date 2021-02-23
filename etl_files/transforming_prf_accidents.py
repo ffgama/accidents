6fde32cec37ea15fc229a426e43199b4d11ec04e
ffgama <f.fabiogama88@gmail.com>
2021-02-23 20:51:53 -0300

    [method]: select desired columns

--- etl_files/transforming_prf_accidents.py
+++ etl_files/transforming_prf_accidents.py
@@ -1,4 +1,5 @@
-from pandas import DataFrame, read_csv, to_numeric, to_datetime
+from pandas import DataFrame, read_csv, to_numeric, to_datetime, set_option
+set_option('display.max_columns', None)
 
 DATA_PATH = 'data/raw_accidents.csv.gz'
 
@@ -7,20 +8,24 @@ open_data.info()
 
 open_data.head(2)
 
-# change data types
-open_data['id'].astype(int)
-
-
+def remove_columns(dataset: DataFrame, columns) -> DataFrame:
+    dataset = dataset.drop(columns=columns, axis=1)
+    return dataset
+    
+open_data = remove_columns(open_data, columns=['id','ano','delegacia','uop'])
 
 # optimization of the columns types
 def data_types_optimization(dataset: DataFrame) -> DataFrame:
+    
     list_types = ['int64','float64']
+        
     int_cols, float_cols = [dataset.select_dtypes(t).columns.to_list() 
                             for t in list_types]
     dataset[int_cols] = dataset[int_cols].apply(to_numeric, downcast='integer')
     dataset[float_cols] = dataset[float_cols].apply(to_numeric, downcast='float')
     
     return dataset
+
 
 data_transf = data_types_optimization(dataset=open_data)
 data_transf.info()
